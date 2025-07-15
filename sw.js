af069e62dd4646e2ab4aca33086aebfb25f6135a
cheesedongjin <129868179+cheesedongjin@users.noreply.github.com>
2025-07-15 15:10:55 +0900

    Add service worker to cache globe texture

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'wheresmyantipode-v1';
+const EARTH_IMG = 'https://unpkg.com/three-globe/example/img/earth-night.jpg';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.add(new Request(EARTH_IMG, { mode: "no-cors" })))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url === EARTH_IMG) {
+    event.respondWith(
+      caches.match(event.request).then(cached => {
+        if (cached) return cached;
+        return fetch(event.request).then(resp => {
+          const respClone = resp.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, respClone));
+          return resp;
+        });
+      })
+    );
+  }
+});
